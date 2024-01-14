64a7cf0b043dab8b91a669f15c1c34b62d095009
C0d3r04 <102381110+C0d3r04@users.noreply.github.com>
2024-01-14 18:49:34 +0530

    Add files via upload
    Completed all functions

--- tree/utils.py
+++ tree/utils.py
@@ -4,76 +4,106 @@ There is no restriction on following the below template, these fucntions are her
 """
 
 import pandas as pd
-import math
-
+import numpy as np
+from scipy.special import xlogy
+from sklearn.model_selection import train_test_split
 
 def check_ifreal(y: pd.Series) -> bool:
     """
     Function to check if the given series has real or discrete values
     """
-
     if y.dtype == int or y.dtype == float:
         return True
+    
     else:
         return False
+    pass
 
 
 def entropy(Y: pd.Series) -> float:
     """
     Function to calculate the entropy
     """
-
-    value_counts = Y.value_counts()
-
-    total_instances = len(Y)
-    entropy = 0
-    for count in value_counts:
-        probability = count / total_instances
-        entropy -= probability * math.log2(probability)
-
-    return entropy
-    
+    prob = Y.value_counts(normalize=True)
+    return -np.sum(prob * np.log2(prob))
+    pass
 
 
 def gini_index(Y: pd.Series) -> float:
     """
     Function to calculate the gini index
     """
+    count = Y.value_counts()
+    prob = count/len(Y)
 
-    value_counts = Y.value_counts()
-
-    total_instances = len(Y)
-    gini_index = 1
-    for count in value_counts:
-        probability = count / total_instances
-        gini_index -= probability**2
-    
+    gini_index = 1 - sum((p**2) for p in prob)
     return gini_index
+    pass
 
 
 def information_gain(Y: pd.Series, attr: pd.Series) -> float:
     """
     Function to calculate the information gain
     """
+    original_entropy = entropy(Y)
 
-    entropy_original = entropy(Y)
-
-    weighted_entropy = 0
+    weighted_sum_entropy = 0
     unique_values = attr.unique()
-    total_instances = len(Y)
 
     for value in unique_values:
-        subset_Y = Y[attr == value]
-        subset_size = len(subset_Y)
-        weight = subset_size / total_instances
-        subset_entropy = entropy(subset_Y)
-        weighted_entropy += weight * subset_entropy
+        subset_indices = attr[attr == value].index
+        subset_entropy = entropy(Y.loc[subset_indices])
+        weighted_sum_entropy += (len(subset_indices) / len(attr)) * subset_entropy
 
-    information_gain = entropy_original - weighted_entropy
+    info_gain = original_entropy - weighted_sum_entropy
 
-    return information_gain
+    return info_gain
+    pass
 
-    
+def mse_calculate(Y: pd.Series) -> float:
+    mean_y = Y.mean()
+    MSE = ((Y - mean_y) ** 2).mean()
+    return MSE
+
+def find_best_split_mse(X: pd.DataFrame, y: pd.Series, feature):
+    unique_values = X[feature].unique()
+    best_split_point = None
+    best_mse = float('inf')
+
+    for value in unique_values:
+        left_indices = X[feature] <= value
+        right_indices = ~left_indices
+
+        left_mse = mse_calculate(y[left_indices])
+        right_mse = mse_calculate(y[right_indices])
+
+        total_mse = left_mse + right_mse
+
+        if total_mse < best_mse:
+            best_mse = total_mse
+            best_split_point = value
+
+    return best_split_point
+
+def find_best_split_cat(X: pd.DataFrame, y: pd.Series, feature):
+    unique_values = X[feature].unique()
+    best_split_point = None
+    best_gini = float('inf')
+
+    for value in unique_values:
+        left_indices = X[feature] == value
+        right_indices = ~left_indices
+
+        left_gini = gini_index(y[left_indices])
+        right_gini = gini_index(y[right_indices])
+
+        total_gini = ((len(y[left_indices]) / len(y))) * left_gini + ((len(y[right_indices]) / len(y))) * right_gini
+
+        if total_gini < best_gini:
+            best_gini = total_gini
+            best_split_point = value
+
+    return best_split_point
 
 
 def opt_split_attribute(X: pd.DataFrame, y: pd.Series, criterion, features: pd.Series):
@@ -89,7 +119,30 @@ def opt_split_attribute(X: pd.DataFrame, y: pd.Series, criterion, features: pd.S
 
     # According to wheather the features are real or discrete valued and the criterion, find the attribute from the features series with the maximum information gain (entropy or varinace based on the type of output) or minimum gini index (discrete output).
 
+    opt_attribute = None
+    best_score = float('inf') 
+
+    if check_ifreal(X[attribute]):
+        for attribute in features:
+            split_point = find_best_split_mse(X, y, attribute)
+            mse = mse_calculate(y[X[attribute] <= split_point]) + mse_calculate(y[X[attribute] > split_point])
+
+            if mse < best_score:
+                best_score = mse
+                opt_attribute = attribute
+
+    else:
+        for attribute in features:
+            split_point = find_best_split_cat(X, y, attribute)
+            gini = gini_index(y[X[attribute] <= split_point]) + gini_index(y[X[attribute] > split_point])
+
+            if gini < best_score:
+                best_score = gini
+                opt_attribute = attribute
+
+    return opt_attribute
     pass
+    
 
 
 def split_data(X: pd.DataFrame, y: pd.Series, attribute, value):
@@ -106,4 +159,12 @@ def split_data(X: pd.DataFrame, y: pd.Series, attribute, value):
 
     # Split the data based on a particular value of a particular attribute. You may use masking as a tool to split the data.
 
+    if check_ifreal(X[attribute]):
+        mask = X[attribute] <= value
+    else:
+        mask = X[attribute] == value
+
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=42, stratify=mask)
+
+    return X_train, X_test, y_train, y_test
     pass
